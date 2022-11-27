a98d6285054f0cc1d7d4b0f30ab7e6952f1e23a4
nicole-hjlin <nicole.hjlin@gmail.com>
2022-11-27 17:35:43 -0500

    update baseline arguments

--- baseline.sh
+++ baseline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-#SBATCH --gres=gpu:1      
+#SBATCH --gres=gpu:3      
 #SBATCH -p seas_gpu_requeue
 #SBATCH -t 15:00:00         # Runtime in D-HH:MM:SS, minimum of 10 minutes
 #SBATCH --mem=6000          # Memory pool for all cores (see also --mem-per-cpu) MBs
@@ -11,4 +11,4 @@ set -x
 
 module load Anaconda3/2020.11
 module load gcc/12.1.0-fasrc01
-python3 -m main --name test --min_points 1000 --learning_rate 1e-4 --epochs 10 --batch_size 64 --data_dir ../data/MpalaForestGEO_LasClippedtoTreePolygons --label_path labels.csv --top_species 5
+python3 -m main --name test --min_points 1000 --learning_rate 1e-4 --epochs 10 --batch_size 64 --data_dir ../data/MpalaForestGEO_LasClippedtoTreePolygons --label_path labels.csv --top_species 3
