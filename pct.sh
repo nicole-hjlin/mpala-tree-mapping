0fc082caca7c743fe4e4dffa4d148b3d5926f17f
nicole-hjlin <nicole.hjlin@gmail.com>
2022-11-28 23:04:28 -0500

    add pct bash script

--- pct.sh
+++ pct.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --gres=gpu:3      
 #SBATCH -p seas_gpu
-#SBATCH -t 02:00:00         # Runtime in D-HH:MM:SS, minimum of 10 minutes
+#SBATCH -t 12:00:00         # Runtime in D-HH:MM:SS, minimum of 10 minutes
 #SBATCH --mem=6000          # Memory pool for all cores (see also --mem-per-cpu) MBs
 #SBATCH -o /n/home10/hongjinlin/outputs/cs288_pct_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e /n/home10/hongjinlin/outputs/cs288_pct_%j.err  # File to which STDERR will be written, %j inserts jobid
@@ -14,4 +14,7 @@ module load cuda/11.7.1-fasrc01
 module load gcc/10.2.0-fasrc01
 pip install ./PCT_Pytorch/pointnet2_ops_lib
 
-python3 -m pct_main --exp_name test --eval True --batch_size 32 --test_batch_size 32 --epochs 1 --num_points 1000 --min_points 1000 --dropout 0.5 --data_dir ../data/MpalaForestGEO_LasClippedtoTreePolygons --label_path labels_test.csv
+python3 -m pct_main --exp_name pct --batch_size 32 --test_batch_size 32 --epochs 20 --num_points 1000 --min_points 1000 --dropout 0.5 --data_dir ../data/MpalaForestGEO_LasClippedtoTreePolygons --label_path labels.csv --output_channels 6 --top_species 5
+python3 -m pct_main --exp_name pct --batch_size 32 --test_batch_size 32 --epochs 20 --num_points 1000 --min_points 1000 --dropout 0.5 --data_dir ../data/MpalaForestGEO_LasClippedtoTreePolygons --label_path labels.csv --output_channels 6 --top_species 5 --lr 1e-5
+
+
