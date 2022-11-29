df79571c9741bfc560cd0fe63eb6f51f4d0bcb2e
nicole-hjlin <nicole.hjlin@gmail.com>
2022-11-29 16:24:26 -0500

    add multiple tests

--- pct.sh
+++ pct.sh
@@ -14,7 +14,11 @@ module load cuda/11.7.1-fasrc01
 module load gcc/10.2.0-fasrc01
 pip install ./PCT_Pytorch/pointnet2_ops_lib
 
-python3 -m pct_main --exp_name pct --batch_size 32 --test_batch_size 32 --epochs 20 --num_points 1000 --min_points 1000 --dropout 0.5 --data_dir ../data/MpalaForestGEO_LasClippedtoTreePolygons --label_path labels.csv --output_channels 6 --top_species 5
-python3 -m pct_main --exp_name pct --batch_size 32 --test_batch_size 32 --epochs 20 --num_points 1000 --min_points 1000 --dropout 0.5 --data_dir ../data/MpalaForestGEO_LasClippedtoTreePolygons --label_path labels.csv --output_channels 6 --top_species 5 --lr 1e-5
+python3 -m pct_main --exp_name pct --batch_size 32 --test_batch_size 32 --epochs 10 --num_points 1000 --min_points 1000 --dropout 0.5 --data_dir ../data/MpalaForestGEO_LasClippedtoTreePolygons --label_path labels.csv --output_channels 6 --top_species 5
+python3 -m pct_main --exp_name pct --batch_size 32 --test_batch_size 32 --epochs 10 --num_points 1000 --min_points 1000 --dropout 0.5 --data_dir ../data/MpalaForestGEO_LasClippedtoTreePolygons --label_path labels.csv --output_channels 6 --top_species 5 --lr 1e-5
+python3 -m pct_main --exp_name pct --batch_size 32 --test_batch_size 32 --epochs 10 --num_points 1000 --min_points 1000 --dropout 0.2 --data_dir ../data/MpalaForestGEO_LasClippedtoTreePolygons --label_path labels.csv --output_channels 6 --top_species 5 --lr 1e-5
+python3 -m pct_main --exp_name pct --batch_size 32 --test_batch_size 32 --epochs 100 --num_points 1000 --min_points 1000 --dropout 0.5 --data_dir ../data/MpalaForestGEO_LasClippedtoTreePolygons --label_path labels.csv --output_channels 6 --top_species 5
+python3 -m pct_main --exp_name pct --batch_size 32 --test_batch_size 32 --epochs 100 --num_points 1000 --min_points 1000 --dropout 0.5 --data_dir ../data/MpalaForestGEO_LasClippedtoTreePolygons --label_path labels.csv --output_channels 6 --top_species 5 --lr 1e-5
+python3 -m pct_main --exp_name pct --batch_size 32 --test_batch_size 32 --epochs 100 --num_points 1000 --min_points 1000 --dropout 0.2 --data_dir ../data/MpalaForestGEO_LasClippedtoTreePolygons --label_path labels.csv --output_channels 6 --top_species 5 --lr 1e-5
 
 
