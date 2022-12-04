85f0bfb0498e6098a06821cdf5f00ba9d4f91c2d
nicole-hjlin <nicole.hjlin@gmail.com>
2022-12-04 15:00:01 -0500

    add baseline models slurm jobs

--- baseline2.sh
+++ baseline2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 
-#SBATCH --gres=gpu:3      
+#SBATCH --gres=gpu:1      
 #SBATCH -p seas_gpu_requeue
-#SBATCH -t 15:00:00         # Runtime in D-HH:MM:SS, minimum of 10 minutes
+#SBATCH -t 18:00:00         # Runtime in D-HH:MM:SS, minimum of 10 minutes
 #SBATCH --mem=6000          # Memory pool for all cores (see also --mem-per-cpu) MBs
 #SBATCH -o /n/home10/hongjinlin/outputs/cs288_baseline_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e /n/home10/hongjinlin/outputs/cs288_baseline_%j.err  # File to which STDERR will be written, %j inserts jobid
@@ -13,4 +13,4 @@ module load Anaconda3/2020.11
 module load gcc/12.1.0-fasrc01
 
 # baseline with same tree same channel
-python3 -m main --name baseline_channel --min_points 1000 --learning_rate 1e-4 --epochs 10 --batch_size 32 --data_dir ../data/MpalaForestGEO_LasClippedtoTreePolygons --label_path labels.csv --top_species 5 --spicy
+python3 -m main --name baseline_channel --min_points 1000 --learning_rate 1e-4 --epochs 100 --batch_size 32 --data_dir ../data/MpalaForestGEO_LasClippedtoTreePolygons --label_path labels.csv --top_species 5 --spicy
